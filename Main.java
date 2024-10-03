ffa5185e45e1b3ebad6d4998ff8fc60f4d04c644
Tiago Lascasas <tiago-antunes@hotmail.com.br>
2024-10-03 16:57:49 -0300

    Revert "progresso ciclos"
    This reverts commit ed93cc906037e1b880ae475b5bf343b3ef90d479.

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.Stack;
 
 class Adjacentes {
@@ -36,7 +33,7 @@ class Adjacentes {
             return cabeca;
         }
 
-        // Selection Sort
+        //Selection Sort
         Adjacentes atual = cabeca;
         while (atual != null) {
             Adjacentes menor = atual;
@@ -92,14 +89,7 @@ class Busca {
     private List<Aresta> retorno;
     private List<List<Integer>> ciclos;
 
-    private Set<String> ciclosSet; // Usado para evitar duplicatas
-
-    private boolean hasCycle = false;
-
-    private Adjacentes[] adjacentes;
-
-    public Busca(Adjacentes[] adjacentes) {
-        this.adjacentes = adjacentes;
+    public Busca(Adjacentes[] adjacentes, int vertice) {
         t = 0;
         td = new int[adjacentes.length];
         tt = new int[adjacentes.length];
@@ -112,9 +102,8 @@ class Busca {
         arvore = new ArrayList<Aresta>();
         retorno = new ArrayList<Aresta>();
         ciclos = new ArrayList<>();
-        ciclosSet = new HashSet<>();
 
-        buscaEmProfundidadeIterativa();
+        buscaEmProfundidadeIterativa(adjacentes);
     }
 
     public int getT(){
@@ -144,30 +133,20 @@ class Busca {
         return retorno;
     }
 
-    public List<List<Integer>> getCiclos() {
-        return ciclos;
-    }
-
-    public boolean hasCycle() {
-        return hasCycle;
-    }
-
-    public void buscaEmProfundidadeIterativa() {
+    public void buscaEmProfundidadeIterativa(Adjacentes[] adjacentes) {
         for (int vInicial = 1; vInicial <= adjacentes.length; vInicial++) {
             if (!visto(vInicial - 1)) {
-                visitaIterativa(vInicial);  // Chama a função de busca para cada nova raiz não visitada
+                visitaIterativa(adjacentes, vInicial);  // Chama a função de busca para cada nova raiz não visitada
             }
         }
     }
 
-    public void visitaIterativa(int vInicial) {
+    public void visitaIterativa(Adjacentes[] adjacentes, int vInicial) {
         Stack<Integer> stack = new Stack<>();
-        Stack<Integer> caminhoAtual = new Stack<>(); // Pilha para o caminho atual
         stack.push(vInicial);
-        caminhoAtual.push(vInicial);
-
+        
         td[vInicial - 1] = ++t;
-
+        
         while (!stack.isEmpty()) {
             int v = stack.peek();
             boolean allVisited = true;
@@ -177,72 +156,40 @@ class Busca {
                 if (!visto(verticeAdj - 1)) {
                     arvore.add(new Aresta(v, verticeAdj));
                     pai[verticeAdj - 1] = v;
-
+                    
                     td[verticeAdj - 1] = ++t;
                     stack.push(verticeAdj);
-                    caminhoAtual.push(verticeAdj);
                     allVisited = false;
                     break;
-                } else if (verticeAdj != pai[v - 1]) {
-                    // Encontrou um ciclo
-                    hasCycle = true;
-
-                    // Reconstrói o ciclo
-                    List<Integer> ciclo = new ArrayList<>();
-
-                    // Start from v, go back through caminhoAtual until we reach verticeAdj
-                    for (int i = caminhoAtual.size() - 1; i >= 0; i--) {
-                        int vertexInPath = caminhoAtual.get(i);
-                        ciclo.add(vertexInPath);
-                        if (vertexInPath == verticeAdj) {
-                            break;
-                        }
-                    }
-
-                    // Normaliza o ciclo
-                    // Reverte para que o ciclo comece do verticeAdj
-                    Collections.reverse(ciclo);
-
-                    // Rotaciona o ciclo para começar com o menor vértice
-                    int minVertex = Collections.min(ciclo);
-                    int minIndex = ciclo.indexOf(minVertex);
-
-                    List<Integer> normalizedCiclo = new ArrayList<>();
-                    for (int i = 0; i < ciclo.size(); i++) {
-                        normalizedCiclo.add(ciclo.get((minIndex + i) % ciclo.size()));
-                    }
-
-                    // Também rotaciona o ciclo invertido
-                    List<Integer> reversedCiclo = new ArrayList<>(ciclo);
-                    Collections.reverse(reversedCiclo);
-                    int minIndexReversed = reversedCiclo.indexOf(minVertex);
-
-                    List<Integer> normalizedReversedCiclo = new ArrayList<>();
-                    for (int i = 0; i < reversedCiclo.size(); i++) {
-                        normalizedReversedCiclo.add(reversedCiclo.get((minIndexReversed + i) % reversedCiclo.size()));
-                    }
-
-                    // Seleciona a versão lexicograficamente menor
-                    List<Integer> finalCiclo = (normalizedCiclo.toString().compareTo(normalizedReversedCiclo.toString()) < 0)
-                        ? normalizedCiclo : normalizedReversedCiclo;
-
-                    // Adiciona o ciclo se ainda não foi registrado
-                    String cicloStr = finalCiclo.toString();
-
-                    if (!ciclosSet.contains(cicloStr)) {
-                        ciclosSet.add(cicloStr);
-                        ciclos.add(finalCiclo);
-                    }
+                } else if (tt[verticeAdj - 1] == 0) {
+                    retorno.add(new Aresta(v, verticeAdj));
                 }
             }
-
+            
             if (allVisited) {
                 tt[v - 1] = ++t;
                 stack.pop();
-                caminhoAtual.pop();
             }
         }
     }
+
+    // public void visitaRecursiva(Adjacentes[] adjacentes, int v) {
+    //     td[v-1] = ++t;
+    //     for (Adjacentes g = adjacentes[v-1]; g != null; g = g.getProximo()) {
+    //         if (!visto(g.getVertice()-1)) {
+    //             arvore.add(new Aresta(v, g.getVertice()));
+    //             pai[g.getVertice()-1] = v;
+    //             visita(adjacentes, g.getVertice());
+    //         } else if (td[g.getVertice()-1] > td[v-1]) {
+    //             avanco.add(new Aresta(v, g.getVertice()));
+    //         } else if (tt[g.getVertice()-1] == 0) {
+    //             cruzamento.add(new Aresta(v, g.getVertice()));
+    //         } else {
+    //             retorno.add(new Aresta(v, g.getVertice()));
+    //         }
+    //     }
+    //     tt[v-1] = ++t;
+    // }
 
     public List<Aresta> getArestasDeArvoreDoVertice(int vertice) {
         List<Aresta> arestasDeArvore = new ArrayList<>();
@@ -256,25 +203,29 @@ class Busca {
     }
 }
 
+class TP1 {
+    public static String identificaCiclos(Adjacentes[] adjacentes) {
+        
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         String nomeArq = null;
         System.out.print("\nDigite o nome do arquivo com sua extensão (.txt): ");
         nomeArq = System.console().readLine();
+        System.out.print("Digite o número do vertice: ");
+        int vertice = Integer.parseInt(System.console().readLine());
         Adjacentes[] adjacentes = null;
         adjacentes = constroiAdjacentes(adjacentes, nomeArq);
-        Busca busca = new Busca(adjacentes);
-
-        if (busca.hasCycle()) {
-            System.out.println("O grafo contém ciclos.");
-            List<List<Integer>> ciclos = busca.getCiclos();
-            System.out.println("Ciclos encontrados:");
-            for (List<Integer> ciclo : ciclos) {
-                System.out.println(ciclo);
-            }
-        } else {
-            System.out.println("O grafo não contém ciclos.");
-        }
+        Busca busca = new Busca(adjacentes, vertice);
+        // System.out.println("Tempo de descoberta: " + busca.getTdAt(vertice-1));
+        // System.out.println("Tempo de término: " + busca.getTtAt(vertice-1));
+        // System.out.println("Tempo total: " + busca.getT());
+        System.out.println("Arestas de árvore: " + busca.getArvore());
+        // System.out.println("Arestas de retorno: " + busca.getRetorno());
+        System.out.println("");
+        System.out.println("Arestas de árvore do vértice " + vertice + ": " + busca.getArestasDeArvoreDoVertice(vertice));
 
     }
 
@@ -286,39 +237,24 @@ public class Main {
 
             adjacentes = new Adjacentes[nVertices];
             for (int i = 0; i < nVertices; i++) {
-                adjacentes[i] = null;
+                adjacentes[i] = new Adjacentes(i+1);
             }
 
             while(rf.getFilePointer() < rf.length()) {
                 int[] valores = processLine(rf);
 
-                int u = valores[0];
-                int v = valores[1];
-
-                // Adiciona v à lista de adjacência de u
-                if (adjacentes[u - 1] == null) {
-                    adjacentes[u - 1] = new Adjacentes(v);
-                } else {
-                    Adjacentes aux = adjacentes[u - 1];
-                    while (aux.getProximo() != null) {
-                        aux = aux.getProximo();
-                    }
-                    aux.setProximo(new Adjacentes(v));
+                Adjacentes aux = adjacentes[valores[0]-1];
+                while (aux.getProximo() != null) {
+                    aux = aux.getProximo();
                 }
-
-                // Adiciona u à lista de adjacência de v (grafo não direcionado)
-                if (adjacentes[v - 1] == null) {
-                    adjacentes[v - 1] = new Adjacentes(u);
-                } else {
-                    Adjacentes aux = adjacentes[v - 1];
-                    while (aux.getProximo() != null) {
-                        aux = aux.getProximo();
-                    }
-                    aux.setProximo(new Adjacentes(u));
+                aux.setProximo(new Adjacentes(valores[1]));
+                aux = adjacentes[valores[1]-1];
+                while (aux.getProximo() != null) {
+                    aux = aux.getProximo();
                 }
+                aux.setProximo(new Adjacentes(valores[0]));
             }
 
-            // Ordena as listas de adjacência
             for (int i = 0; i < nVertices; i++) {
                 adjacentes[i] = Adjacentes.ordenarAdjacentes(adjacentes[i]);
             }            
@@ -327,7 +263,7 @@ public class Main {
 
             return adjacentes;
         } catch (Exception e) {
-            System.out.println("Erro ao abrir o arquivo: " + e.getMessage());
+            System.out.println("Erro ao abrir o arquivo");
             return null;
         }
     }
@@ -341,8 +277,9 @@ public class Main {
             values[0] = Integer.parseInt(numeros[0]);
             values[1] = Integer.parseInt(numeros[1]);
         } catch (Exception e) {
-            System.out.println("Erro ao ler a linha: " + e.getMessage());
+            System.out.println("Erro ao ler a linha: " + e);
         }
         return values;
     }
 }
+
